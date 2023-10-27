fa0c305a5ea6e312784007db8573bb3a26833f61
trev-git <htaedshararam@gmail.com>
2023-10-27 12:06:07 +0300

    hsdjljdsflksjdfklsdf

--- security/2023-10-04.py
+++ security/2023-10-04.py
@@ -1,12 +1,17 @@
-text = 'MHILY LZA ZBHL XBPZXBL MVYABUHL HWWPBZ JSHBKPBZ JHLJBZ KPJABT HYJHUBT LZA ULBAYVU'
+import sys
 
+if len(sys.argv) < 3 or len(sys.argv) > 3:
+    print("идинахуй")
+    exit(0)
+
+text = sys.argv[1].upper()
+shift = int(sys.argv[2])
 res = ''
 
 for ch in text:
     if ch == ' ':
         res += ch
         continue
-
-    res += chr((ord(ch)+6) % 26 + ord('A'))
+    res += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
 
 print(res)
---
+++ security/2023-10-27.py
@@ -0,0 +1,2 @@
+from numpy.linalg import *
+
